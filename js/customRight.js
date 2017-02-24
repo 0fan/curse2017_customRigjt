e8c7a58e7348aabdcf749023d614c938ae40afab
linfan <dear_moo@qq.com>
2017-02-24 21:14:18 +0800

    基本完成任务所要求的功能

---
+++ js/customRight.js
@@ -0,0 +1,31 @@
+document.addEventListener('contextmenu',function(e){
+
+	e.preventDefault();
+
+	var customRight = document.querySelector('.customRight'),
+		container   = document.querySelector('.customRight-block'),
+		containerX  = container.offsetLeft,
+		containerY  = container.offsetTop,
+		containerW  = container.offsetWidth,
+		containerH  = container.offsetHeight,
+		x           = e.pageX,
+		y           = e.pageY;
+
+	if( x >= containerX && x <= containerX + containerW && y >= containerY && y <= containerY + containerH ){
+		customRight.style.left    = x + 'px';
+		customRight.style.top     = y + 'px';
+		customRight.style.display = 'block';
+	}else{
+		customRight.style.display = 'none';
+	}
+
+});
+
+document.addEventListener('click',function(e){
+	if(e.button === 2){
+		return;
+	}
+
+	customRight = document.querySelector('.customRight');
+	customRight.style.display = 'none';
+});
