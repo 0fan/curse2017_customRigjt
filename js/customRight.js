80a7713bcc1fa839217b996a53082b57db91181d
linfan <dear_moo@qq.com>
2017-02-24 21:22:45 +0800

    不知道为什么github预览的js不能执行，明明加载进去了，我把代码重新放在了window.onload里试试

--- js/customRight.js
+++ js/customRight.js
@@ -1,31 +1,33 @@
-document.addEventListener('contextmenu',function(e){
+window.onload = function(){
+	document.addEventListener('contextmenu',function(e){
 
-	e.preventDefault();
+		e.preventDefault();
 
-	var customRight = document.querySelector('.customRight'),
-		container   = document.querySelector('.customRight-block'),
-		containerX  = container.offsetLeft,
-		containerY  = container.offsetTop,
-		containerW  = container.offsetWidth,
-		containerH  = container.offsetHeight,
-		x           = e.pageX,
-		y           = e.pageY;
+		var customRight = document.querySelector('.customRight'),
+			container   = document.querySelector('.customRight-block'),
+			containerX  = container.offsetLeft,
+			containerY  = container.offsetTop,
+			containerW  = container.offsetWidth,
+			containerH  = container.offsetHeight,
+			x           = e.pageX,
+			y           = e.pageY;
 
-	if( x >= containerX && x <= containerX + containerW && y >= containerY && y <= containerY + containerH ){
-		customRight.style.left    = x + 'px';
-		customRight.style.top     = y + 'px';
-		customRight.style.display = 'block';
-	}else{
+		if( x >= containerX && x <= containerX + containerW && y >= containerY && y <= containerY + containerH ){
+			customRight.style.left    = x + 'px';
+			customRight.style.top     = y + 'px';
+			customRight.style.display = 'block';
+		}else{
+			customRight.style.display = 'none';
+		}
+
+	});
+
+	document.addEventListener('click',function(e){
+		if(e.button === 2){
+			return;
+		}
+
+		customRight = document.querySelector('.customRight');
 		customRight.style.display = 'none';
-	}
-
-});
-
-document.addEventListener('click',function(e){
-	if(e.button === 2){
-		return;
-	}
-
-	customRight = document.querySelector('.customRight');
-	customRight.style.display = 'none';
-});
+	});
+}
